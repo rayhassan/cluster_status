e6747c2a1e17fd668ab5c29c38742aafbe795ec8
Ray Hassan <rayhassan@users.noreply.github.com>
2016-05-05 14:33:05 +0100

    Added files via upload

--- cluster_status.py
+++ cluster_status.py
@@ -17,13 +17,10 @@ def configure_parser():
                     help='Cluster or CVM host/IP address')
     return arg_parser
 
-class RestApi():
+class PrismRestApi():
     def __init__(self, ip_addr, user, passwd):
-        #self.serverIpAddress = "10.68.64.55"
         self.serverIpAddress = ip_addr
-        #self.username = "admin"
         self.username = user
-        #self.password = "nutanix/4u"
         self.password = passwd
         BASE_URL = 'https://%s:9440/PrismGateway/services/rest/v1'
         self.base_url = BASE_URL % self.serverIpAddress
@@ -45,22 +42,21 @@ class RestApi():
 
 
 if __name__ == "__main__":
+
+    parser = configure_parser()
+    args = parser.parse_args()
+
+    ipaddr = args.ip
+    user = args.user
+    password = args.password
+
     try:
-
-        parser = configure_parser()
-        args = parser.parse_args()
-
-        ipaddr = args.ip
-        user = args.user
-        password = args.password
-
-        client = RestApi(ipaddr, user, password)
+        client = PrismRestApi(ipaddr, user, password)
         status, cluster = client.getClusterInformation()
 
         pp = pprint.PrettyPrinter(indent=2)
         print ("=" * 79)
         print "Status: %s" % status
-        #print "Text:"
         #pp.pprint(cluster)
         print ("=" * 79) 
         print "Name: %s" % cluster.get('name')
@@ -69,22 +65,21 @@ if __name__ == "__main__":
         print "Number of nodes: %s" % cluster.get('numNodes')
         print "Version: %s" % cluster.get('version')
         print "Hypervisor Types: %s" % cluster.get('hypervisorTypes')
-        #print "Cluster current redundancy factor: %s" % cluster.get('clusterRedundancyState')
         current_RF = cluster["clusterRedundancyState"]["currentRedundancyFactor"]
         desired_RF = cluster["clusterRedundancyState"]["desiredRedundancyFactor"]
         if current_RF == desired_RF:
             print "Current redundancy factor (%s) == Desired redundancy factor (%s) : OK" % (current_RF, desired_RF)
         else: 
-            print "Current redundancy factor (%s) != Desired redundancy factor (%s) : Not OK!" % (current_RF, desired_RF)
+            print "Current redundancy factor (%s) != Desired redundancy factor (%s) Not OK!" % (current_RF, desired_RF)
 
         zk_status = cluster["clusterRedundancyState"]["redundancyStatus"]["kZookeeperPrepareDone"] 
         if zk_status == True:
-            print "Zookeeper status: OK"
+            print "Zookeeper FT status: OK"
         else: print "Zookeeper status not OK"
  
-        cassandra_stat = cluster["clusterRedundancyState"]["redundancyStatus"]["kCassandraPrepareDone"]
-        if cassandra_stat == True:
-            print "Cassandra status: OK"
+        cassandra_status = cluster["clusterRedundancyState"]["redundancyStatus"]["kCassandraPrepareDone"]
+        if cassandra_status == True:
+            print "Cassandra FT status: OK"
         else: print "Cassandra status not OK"
 
         print ("=" * 79) 
